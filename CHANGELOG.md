86ba162efba4776007e9f0dbc2c9ab6f721495c2
Toby Allen <toby@cookingisfun.ie>
2025-12-30 09:35:09 +0000

    update to WebflowSiteConverter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 # Changelog
 
-All notable changes to `:package_name` will be documented in this file.
+All notable changes to `WebflowSiteConverter` will be documented in this file.
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Tobya\WebflowSiteConverter\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
