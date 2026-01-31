a51ee518e616749743985832c537c98a576b9e73
Toby Allen <toby@cookingisfun.ie>
2026-01-31 08:18:36 +0000

    update config html

--- config/webflow-site-converter.php
+++ config/webflow-site-converter.php
@@ -1,19 +1,37 @@
 <?php
 
-// config for Tobya/WebflowSiteConverter
+
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Site Transformer
+    |--------------------------------------------------------------------------
+    |
+    | The class that is used to transform your site files.
+    | By Default this is the built in SiteTransformer
+    | If you with to costomise you can provide
+    | a class that descends from this.
+    |
+    */
 
-    'transformer' => \Tobya\WebflowSiteConverter\Transformers\SiteTransformer::class,
+    'blade_transformer' => \Tobya\WebflowSiteConverter\Transformers\SiteTransformer::class,
+
+    'html_transformer' => \Tobya\WebflowSiteConverter\Transformers\HtmlTransformer::class,
 
 
-    'sections' => [
 
-        'extracted-sections-hashed-directory' => 'extracted',
-
-        'extracted-sections-blades' => 'sections',
-    ],
-
+    /*
+    |--------------------------------------------------------------------------
+    | Disks
+    |--------------------------------------------------------------------------
+    |
+    | The transformation requires 3 seperate disk drivers.  The Input where
+    | your site files are.  Output where the tranformed fiiles will be
+    | by default this is the '/views' directory, and public where
+    | all css, js and images files will be put.
+    |
+    */
 
     'disks' => [
         'input' => [  // disk name or array for build.
@@ -25,10 +43,25 @@ return [
         'output' => [
 
             'driver' => 'local',
-            'root' => resource_path('transformed'),
+            'root' => resource_path('/views'),
             ] ,
-        'public' => 'public', // Use Public disk.
+        'public' =>  // Use root Public dir.
+             [
+
+                'driver' => 'local',
+                'root' => public_path('/'),
+                ] ,
         ],
+
+
+
+    'sections' => [
+
+        'extracted-sections-hashed-directory' => 'extracted',
+
+        'extracted-sections-blades' => 'sections',
+    ],
+
 
 
 ];
